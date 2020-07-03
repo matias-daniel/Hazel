0b59afd8c639b7242f4b1c91e368a0d21cdad948
daniel <danielelcapo0000@gmail.com>
2020-07-03 13:58:47 -0300

    Add a logger

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,6 +3,7 @@
 // For use by hazel application
 
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 // ---Entry Point-------------------------------------------
 #include "Hazel/EntryPoint.h"
